04b46eaa14fbd0e808ad6bfaab8c8c76cc3edc09
Marjorie Ready <mlready@tds.net>
2012-09-03 20:53:11 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iconsult::Application.config.secret_token = 'fa68f0da0864ab435926bc7e6e0d4a8452f9e150faa54b5d95eff3199cd59efdfb83cf3eb64db9b6cb3622a95b82880a3be688958992687ab4b153e818509f83'
